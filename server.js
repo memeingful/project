8a916b4a256cfed3f562caf64644af38d63c8f74
jackie <jcqnly@gmail.com>
2018-04-26 22:18:07 -0700

    testing routes

--- server.js
+++ server.js
@@ -2,19 +2,19 @@
 require('dotenv').config();
 // Application dependencies
 const express = require('express');
-const request = require('request');
+// const request = require('request');
 
 // Application Setup
 const app = express();
 const PORT = process.env.PORT;
-const CLIENT_URL = process.env.CLIENT_URL;
+// const CLIENT_URL = process.env.CLIENT_URL;
 
 // Database Setup
-const client = new pg.Client(process.env.DATABASE_URL);
-client.connect();
-client.on('error', err => console.error(err));
+// const client = new pg.Client(process.env.DATABASE_URL);
+// client.connect();
+// client.on('error', err => console.error(err));
 
-app.use(express.urlencoded({extended:true}));
+// app.use(express.urlencoded({extended:true}));
 //add API and JSON line of code from lecture here
 
 // app.get('*', (req, res) => res.redirect(CLIENT_URL));
