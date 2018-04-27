3f04dee327ac8f0291bdb7b701ad1302ce857ffa
jackie <jcqnly@gmail.com>
2018-04-26 22:21:14 -0700

    add test to push to github

--- server.js
+++ server.js
@@ -20,3 +20,4 @@ const PORT = process.env.PORT;
 // app.get('*', (req, res) => res.redirect(CLIENT_URL));
 app.get('/', (req, res) => res.send('hi'));
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
+//test
