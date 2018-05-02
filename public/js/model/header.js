cde10b0f858dd100f5e941d438c32b574a1aac68
SeatownAC <33205876+SeatownAC@users.noreply.github.com>
2018-05-02 16:43:19 -0700

    added touchstart to mobile

--- public/js/model/header.js
+++ public/js/model/header.js
@@ -1,19 +1,20 @@
 'use strict';
 
-// function startup() {
-//     var el = document.getElementsByTagName("")[0];
-//     el.addEventListener("touchstart", handleStart, false);
-//     el.addEventListener("touchend", handleEnd, false);
-//     el.addEventListener("touchcancel", handleCancel, false);
-//     el.addEventListener("touchmove", handleMove, false);
-//     log("initialized.");
-//   }
+$('header').bind('touchstart', function() {});
 
-//   startup();
 
-$(document).ready(function() {
-    $('.hover').bind('touchstart touchend', function(e) {
-        e.preventDefault();
-        $(this).toggleClass('hover_effect');
-    });
-});
+
+// $(document).ready(function() {
+//     $('.hover').bind('touchstart touchend', function(e) {
+//         e.preventDefault();
+//         $(this).toggleClass('hover_effect');
+//     });
+// });
+
+
+
+// jQuery(document).ready(function() {
+//     jQuery('ul.nav-bar').superfish();
+// });
+
+
